94056d3bcb94f8f5fc51bef3f8b8d4b4b1f03a01
Sujan Molla <smitexpert@gmail.com>
2023-08-21 13:34:25 +0600

    Update | Service Class

--- src/TripleaServiceProvider.php
+++ src/TripleaServiceProvider.php
@@ -6,6 +6,11 @@ use Illuminate\Support\ServiceProvider;
 
 class TripleaServiceProvider extends ServiceProvider {
 
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/triplea.php', 'triplea');
+    }
+
     public function boot() {
 
         $this->publishes([
